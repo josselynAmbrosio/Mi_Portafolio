00ae660fa51b7adcf609b97c64e35613d3dc5d54
josselynAmbrosio <josselynambrosioespiritu@gmail.com>
2015-05-11 03:18:02 +0100

    algunos canbios

--- javascripts/efects.js
+++ javascripts/efects.js
@@ -1,10 +1,10 @@
 $(window).scroll(function(){
 	var navegador= $(this).scrollTop();
-	if (navegador > 899){
+	if (navegador > 400){
 		$(".navbar").addClass("scroll");
 		$(".navbar-brand").removeClass("hidden")
 	}
-	else if (navegador < 800){
+	else if (navegador < 550){
 		$(".navbar").removeClass("scroll");
 		$(".navbar-brand").addClass("hidden")
 
