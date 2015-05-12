67c02788e3c78049d5ff7eb3b1f61f129c63cc18
josselynAmbrosio <josselynambrosioespiritu@gmail.com>
2015-05-12 04:08:17 +0100

    más canbios

--- javascripts/efects.js
+++ javascripts/efects.js
@@ -1,12 +1,12 @@
-$(window).scroll(function(){
-	var navegador= $(this).scrollTop();
-	if (navegador > 400){
-		$(".navbar").addClass("scroll");
-		$(".navbar-brand").removeClass("hidden")
-	}
-	else if (navegador < 550){
-		$(".navbar").removeClass("scroll");
-		$(".navbar-brand").addClass("hidden")
+// $(window).scroll(function(){
+// 	var navegador= $(this).scrollTop();
+// 	if (navegador > 400){
+// 		$(".navbar").addClass("scroll");
+// 		$(".navbar-brand").removeClass("hidden")
+// 	}
+// 	else if (navegador < 550){
+// 		$(".navbar").removeClass("scroll");
+// 		$(".navbar-brand").addClass("hidden")
 
-	}
-});
+// 	}
+// });
